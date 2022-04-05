ab47f0783d50754e0b90f45b22602ff6a04cd286
Frédéric Alphonse <101244166+Fred-23@users.noreply.github.com>
2022-04-05 12:01:56 +0000

    Version qui a été rendu la première fois pour le tp 8

--- Animaux/ours.cpp
+++ Animaux/ours.cpp
@@ -7,48 +7,40 @@ void Ours::setAttaque() {
   this->typeAttaque = temp;
 }
 
-//(2,1), (1,2), (-1,2), (-2,1), (-2,-1), (-1,-2), (1,-2), (2, -1)
 
 void Ours::deplace(int maxX, int maxY) {
   srand(time(NULL));
   int ind_pos=rand()%(8);
-  
+  //(2,1), (1,2), (-1,2), (-2,1), (-2,-1), (-1,-2), (1,-2), (2, -1)
   switch(ind_pos) {
     case 0:
       this->x += 2;
       this->y += 1;
       break;
-
     case 1:
       this->x += 1;
       this->y += 2;
       break;
-    
     case 2:
       this->x -= 1;
       this->y += 2;
       break;
-
     case 3:
       this->x -= 2;
       this->y += 1;
       break;
-    
     case 4:
       this->x -= 2;
       this->y -= 1;
       break;
-
     case 5:
       this->x -= 1;
       this->y -= 2;
       break;
-
     case 6:
       this->x += 1;
       this->y -= 2;
       break;
-
     case 7:
       this->x += 2;
       this->y -= 1;
@@ -61,7 +53,7 @@ void Ours::deplace(int maxX, int maxY) {
   else {
     this->x = (this->x)%maxX;
   }
-
+  
   if(this->y<0){
     this->y = maxY-(this->y);
   }
--- animal.h
+++ animal.h
@@ -27,7 +27,6 @@ public:
   bool attaque(Animal &a);
   virtual void setAttaque()=0; //virtuelle pure
   virtual void deplace(int maxX, int maxY)=0;//virtuelle pure
-  
 };
 
 #endif
